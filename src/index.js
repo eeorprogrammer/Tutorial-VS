df0e8c43e7527d2fdf4d321bebf8592e4f062857
edgar <edgaremilio83@gmail.com>
2022-10-22 20:37:21 -0500

    Funcionamiento Debbuger 2

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const fn =  () => {
+    const miString = "soy un String";
+    let b = miString + "muy feliz";
+   console.log(b);
+}
+
+fn();
+
+const clickeando = () =>
+{
+    let a = "humano feliz";
+    console.log(a);
+}
